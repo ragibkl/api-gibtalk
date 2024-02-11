97a58cdc1692d8a932f73fe3a353d8a9fe0dba50
Ragib Badaruddin <ragib.badaruddin@gmail.com>
2024-02-11 15:42:30 +0800

    cors allow all

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,16 @@
-use axum::{
-    routing::get,
-    extract::Query,
-    Router,
-    Json,
-};
-use rust_search::{SearchBuilder, similarity_sort};
+use axum::{extract::Query, routing::get, Json, Router};
+use http::Method;
+use rust_search::{similarity_sort, SearchBuilder};
 use serde::{Deserialize, Serialize};
-use tower_http::services::ServeDir;
+use tower_http::{
+    cors::{Any, CorsLayer},
+    services::ServeDir,
+};
 use url::Url;
 
 #[derive(Serialize, Deserialize, Debug)]
 struct SymbolsSearchQuery {
-    q: String
+    q: String,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -20,9 +19,7 @@ struct SymbolOutput {
 }
 
 #[axum_macros::debug_handler]
-async fn symbols_search(
-    Query(query): Query<SymbolsSearchQuery>,
-) -> Json<Vec<SymbolOutput>> {
+async fn symbols_search(Query(query): Query<SymbolsSearchQuery>) -> Json<Vec<SymbolOutput>> {
     let mut search: Vec<String> = SearchBuilder::default()
         .location("./media")
         .search_input(&query.q)
@@ -38,17 +35,25 @@ async fn symbols_search(
     let base_url = std::env::var("BASE_URL").unwrap_or("http://localhost:3000/".to_string());
     let base_url = Url::parse(&base_url).unwrap();
 
-    let result = search.into_iter().map(|s| {
-        let url = base_url.join(&s).unwrap().to_string();
-        SymbolOutput { url }
-    }).collect();
+    let result = search
+        .into_iter()
+        .map(|s| {
+            let url = base_url.join(&s).unwrap().to_string();
+            SymbolOutput { url }
+        })
+        .collect();
 
     Json(result)
 }
 
 #[tokio::main]
 async fn main() {
+    let cors = CorsLayer::new()
+        .allow_methods(vec![Method::GET, Method::POST])
+        .allow_origin(Any);
+
     let app = Router::new()
+        .layer(cors)
         .route("/api/symbols/search/", get(symbols_search))
         .nest_service("/media/", ServeDir::new("media"));
 
